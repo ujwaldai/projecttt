72359b02a62e4832e7f9688e1199d4c76e7c6eaf
AMAN <amandangol423@gmail.com>
2022-04-22 09:14:37 +0545

    asd

--- Main Project/DMS/DMS.UI/Controllers/Main/loginController.cs
+++ Main Project/DMS/DMS.UI/Controllers/Main/loginController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using DMS.DAL.DatabaseContext;
+using DMS.DAL.Interfaces;
+using DMS.DAL.StaticHelper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,7 +13,21 @@ namespace DMS.Controllers
     public class loginController : Controller
     {
         // GET: login
-        public ActionResult Admin_Home()
+        private MainEntities db;
+        private SystemInfoForSession _ActiveSession;
+        private IBranchesRepo _BranchesRepo;
+
+        public loginController(MainEntities _db, IBranchesRepo BranchesRepo)
+        {
+            _ActiveSession = SessionHelper.GetSession();
+            db = _db;
+            _BranchesRepo = BranchesRepo;
+        }
+        public ActionResult Admin_Index()
+        {
+            return View();
+        }
+        public ActionResult Admin_froms()
         {
             return View();
         }
